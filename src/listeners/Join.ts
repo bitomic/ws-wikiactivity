a7dbb982384bb7ced7f0753e20578b980dab4c9c
bitomic <35199700+bitomic@users.noreply.github.com>
2022-06-18 16:05:10 -0600

    feat: join listener

---
+++ src/listeners/Join.ts
@@ -0,0 +1,14 @@
+import { ApplyOptions, Listener, type ListenerOptions, pino, io } from '../lib'
+import type { Socket } from 'socket.io'
+
+@ApplyOptions<ListenerOptions>( {
+	event: 'join',
+	type: 'client'
+} )
+export class CustomListener extends Listener {
+	public async run( socket: Socket, rooms: string[] ): Promise<void> {
+		const existingRooms = io.sockets.adapter.rooms
+		await socket.join( rooms )
+		pino.info( [ ...existingRooms.keys() ].join( ', ' ) )
+	}
+}
