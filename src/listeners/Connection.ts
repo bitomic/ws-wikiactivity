2eca9e995f155a22d4c1fb84320fa67b66f097a4
bitomic <35199700+bitomic@users.noreply.github.com>
2022-06-18 16:05:00 -0600

    feat: register client listeners

--- src/listeners/Connection.ts
+++ src/listeners/Connection.ts
@@ -1,11 +1,20 @@
-import { ApplyOptions, Listener, type ListenerOptions, pino } from '../lib'
+import { ApplyOptions, Listener, type ListenerOptions, pino, listeners } from '../lib'
 import type { Socket } from 'socket.io'
 
 @ApplyOptions<ListenerOptions>( {
-	event: 'connection'
+	event: 'connection',
+	type: 'server'
 } )
 export class CustomListener extends Listener {
 	public run( socket: Socket ): void {
+		socket.emit( 'connection' )
 		pino.info( `Connection: ${ socket.id }` )
+
+		socket.on( 'message', async (event: string, ...args: unknown[]) => {
+			const clientListeners = listeners.filter( listener => listener.options.event === event )
+			for ( const listener of clientListeners.values() ) {
+				await listener.run( socket, ...args )
+			}
+		} )
 	}
 }
