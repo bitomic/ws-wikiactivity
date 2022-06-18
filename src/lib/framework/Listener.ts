44a19e9717b911090d665585c726eb3def98409c
bitomic <35199700+bitomic@users.noreply.github.com>
2022-06-18 16:04:19 -0600

    fix: distinguish client and server listeners

--- src/lib/framework/Listener.ts
+++ src/lib/framework/Listener.ts
@@ -2,9 +2,10 @@ import { Piece, type PieceOptions } from '@sapphire/pieces'
 import type { Socket } from 'socket.io'
 
 export abstract class Listener extends Piece<ListenerOptions> {
-	public abstract run( socket: Socket ): void | Promise<void>
+	public abstract run( socket: Socket, ...args: unknown[] ): void | Promise<void>
 }
 
 export interface ListenerOptions extends PieceOptions {
 	event: string
+	type: 'server' | 'client'
 }
